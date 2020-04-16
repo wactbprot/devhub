27931524d0734685ed61d692b57840ba71d52015
Wact B. Prot <wactbprot@gmail.com>
2020-04-16 14:14:37 +0200

    rdm examples

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # devhub
 
 Now a stub later a hub.
+
+
+## curl examples
+
+### no taskname an action
+
+```shell
+curl -d '{"Missing":true}' -H "Content-Type: application/json"  -X POST http://localhost:8008 -i
+## HTTP/1.1 404 Not Found
+## Date: Thu, 16 Apr 2020 12:07:21 GMT
+## Content-Type: application/json;charset=utf-8
+## Content-Length: 70
+## Server: Jetty(9.4.12.v20180830)
+## 
+## {"error":"body don't contain a action, body don't contain a taskname"}
+```
+
+### no matches
+
+```shell
+curl -i -d '{"Action":"foo","TaskName":"bar"}' -H "Content-Type: application/json"  -X POST http://localhost:8008
+#
+## HTTP/1.1 404 Not Found
+## Date: Thu, 16 Apr 2020 11:32:35 GMT
+## Content-Type: application/json;charset=utf-8
+## Content-Length: 61
+## Server: Jetty(9.4.12.v20180830)
+## 
+##{"error":"no edn for action: foo, no edn for task name: xyz"}
+```
+
+### action match
+
+```shell
+curl -d '{"Action":"MODBUS"}' -H "Content-Type: application/json"  -X POST http://localhost:8008 -i
+## HTTP/1.1 200 OK
+## Date: Thu, 16 Apr 2020 11:54:46 GMT
+## Content-Type: application/json;charset=utf-8
+## Content-Length: 147
+## Server: Jetty(9.4.12.v20180830)
+## 
+## {"ToExchange":
+## {"V1":
+## {"Bool":1},
+## "Vraw_block1":[1,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0]},
+## "t_start":"1587038086886",
+## "t_stop":"1587038086916"}
+```
--- src/devhub/core.clj
+++ src/devhub/core.clj
@@ -5,15 +5,68 @@
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.util.response :as res]))
 
-(defonce srv (atom nil))
+(defn file-name
+  [s]
+  (if (string? s)
+    (str "resources/" (lower-case s) ".edn")))
 
-(defn handler [req]
-  (if-let [task-name (get-in req [:body :TaskName])]
-    (let [file-name (str "resources/" (lower-case task-name) ".edn")]
-      (if (.exist (as-file file-name))
-        (res/response (read-string (slurp file-name)))
-        (res/bad-request {:error (str "unknown task name: " task-name)})))
-    (res/bad-request {:error "body don't contain a taskname"})))
+(defn file?
+  [f]
+  (.exists (as-file f)))
+
+(defn ms
+  []
+  (str (inst-ms (java.util.Date.))))
+
+(defn by-name
+  [req]
+  (if-let [n (get-in req [:body :TaskName])]
+    (let [f (file-name  n) ]
+      (if (file? f) 
+        (try
+          {:data (read-string (slurp f))}
+           (catch Exception e
+             {:error (.getMessage e)}))
+        {:msg (str "no edn for task name: " n)}))
+    {:msg "body don't contain a taskname"}))
+
+(defn by-action
+  [req]
+  (if-let [n (get-in req [:body :Action])]
+    (let [f (file-name n)]
+      (if (file? f)
+        (try
+          {:data (read-string (slurp f))}
+           (catch Exception e
+             {:error (.getMessage e)}))
+        {:msg (str "no edn for action: " n)}))
+    {:msg "body don't contain a action"}))
+
+(defn handler
+  [req]
+  (let [t-start     (ms)
+        m-name      (by-name req)
+        m-action    (by-action req)
+        msg-name    (:msg m-name)
+        msg-action  (:msg m-action)
+        data-name   (:data m-name)
+        data-action (:data m-action)]
+    (cond
+      (:error m-name)     (res/bad-request m-name)
+      (:error m-action)   (res/bad-request m-action)
+      (and
+       msg-name
+       msg-action)        (res/not-found {:error (str msg-action
+                                                     ", "
+                                                     msg-name)})
+      (map? data-name)    (res/response
+                           (assoc data-name
+                                 :t_start t-start
+                                 :t_stop (ms)))
+      (map? data-action)  (res/response
+                           (assoc data-action
+                                  :t_start t-start
+                                  :t_stop (ms))))))
 
 (def app
   (wrap-json-response
