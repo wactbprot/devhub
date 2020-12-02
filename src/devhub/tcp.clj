f84d22aca64b16c85921c53e673753f25eb21654
Thomas Bock <thomas.bock@ptb.de>
2020-12-02 14:26:52 +0100

    works with serial servers

--- src/devhub/tcp.clj
+++ src/devhub/tcp.clj
@@ -3,19 +3,34 @@
    [ring.util.response :as res]
    [devhub.utils       :as u])
   (:import
-   [java.io DataInputStream DataOutputStream]
+   [java.io DataInputStream OutputStreamWriter PrintWriter]
    [java.net Socket]))
 
-(defn send-request
+(defn send-receive
+  "Sends the command `cmd` to the `out`put-stream. Receives data
+  from the `in`put-stream. Wraps time stamps around."
+  [in out cmd]
+  (.print out cmd)
+  (.flush out)
+  (let [t0  (u/ms)
+        res (.readLine in)
+        t1  (u/ms)]
+    (u/add-times {:_x res} t0 t1)))
+
+(defn query
   "Sends an `cmd` a raw tcp socket with the specified `host` and
   `port`."
-  [host port cmd]
-  (let [t0 (u/ms)]
-    (with-open [sock (Socket. host port)
-                out  (DataOutputStream. (.getOutputStream sock))
-                in   (DataInputStream. (.getInputStream sock))]
-      (.writeUTF out cmd)
-      (u/add-times {:_x (.readLine in)} t0 (u/ms)))))
+  [host port cmds wait repeat]
+  (with-open [sock (Socket. host port)
+              out (PrintWriter. (OutputStreamWriter. (.getOutputStream sock)))
+              in  (DataInputStream. (.getInputStream sock))]
+    (mapv (fn [_]
+            (let [v (mapv (fn [cmd]
+                            (send-receive in out cmd))
+                          cmds)]
+              (Thread/sleep wait)
+              v))
+      (range repeat))))
 
 (defn handler
   "Handles TCP requests.
@@ -28,6 +43,10 @@
   ```"
   [{w :Wait r :Repeat p :Port h :Host v :Value }]
   (if (and v h p )
-    (res/response (send-request h p v))
+    (res/response (query h
+                         (u/number p)
+                         (if (string? v) [v] v)
+                         (if w (u/number w) 10)
+                         (if r (u/number r) 1)))
     (res/response {:error true :reason "no <value>, <host> or <port> given"})))
   
--- src/devhub/utils.clj
+++ src/devhub/utils.clj
@@ -1,8 +1,29 @@
+
 (ns devhub.utils
   (:require [clojure.string  :refer [lower-case]]
             [clojure.pprint  :as pp]
             [clojure.edn     :as edn]
             [clojure.java.io :refer [as-file]]))
+ 
+(defn number
+  "Ensures the `x` to be a `number` or `nil`.
+
+  ```clojure
+  (number 1234)
+  ;; =>
+  ;; 1234
+  (number true)
+  ;; =>
+  nil
+  (number \"1234\")
+  ;; =>
+  1234
+  ```
+  "
+  [x]
+  (cond
+    (string? x) (edn/read-string x)
+    (number? x) x))
 
 (defn file-name [s] (when (string? s) (str "resources/" (lower-case s) ".edn")))
 
@@ -16,27 +37,12 @@
 
 (defn task-name [req] (:TaskName (task req)))
 
-(defn add-times [m t0 t1] (assoc m :t_start t0 :t_stop t1))
-
-(defn port
-  "Ensures the `p`ort to be a `number` or `nil`.
-
-  ```clojure
-  (port 1234)
-  ;; =>
-  ;; 1234
-  (port true)
-  ;; =>
-  nil
-  (port \"1234\")
-  ;; =>
-  1234
-  ```
-  "
-  [p]
-  (cond
-    (string? p) (edn/read-string p)
-    (number? p) p))
+(defn add-times
+  [m t0 t1]
+  (assoc m
+         :t_start t0
+         :t_stop t1
+         :dt (- (number t1) (number t0))))
 
 (defn file-content
   [f]
