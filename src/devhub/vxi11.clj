13900185eddb0670301f48f01da7343e4c5553bb
Thomas Bock <thomas.bock@ptb.de>
2020-12-07 15:23:22 +0100

    vxi ..

--- src/devhub/vxi11.clj
+++ src/devhub/vxi11.clj
@@ -6,12 +6,10 @@
 
 (defn parse-gpib-str
   [s]
-  (let [v (re-matcher #"gpib([0-9]*),([0-9]*)" s)]
-    (if (empty? v)
-      {:device "inst0" :pri-adr -1 :sec-adr -1} 
-      {:device "inst0"
-       :pri-adr (u/number (nth v 1))
-       :sec-adr (u/number (nth v 2))})))
+  (let [v (re-find (re-matcher #"(gpib[0-9]*),([0-9]*)" s))]
+    {:device (nth v 1)
+     :pri-adr (u/number (nth v 2))
+     :sec-adr 0}))
 
 (defn parse-inst-str [s] {:device s :pri-adr -1 :sec-adr -1})
       
@@ -21,26 +19,34 @@
     (string/starts-with? s "inst") (parse-inst-str s)
     (string/starts-with? s "gpib") (parse-gpib-str s)
     :else (throw "imprement me")))
-  
-(defn query
-  [h d v w r]
 
-  (let [adr  (parse-device-str d)
-        ctrl (VXI11Factory/create  h (:device adr))
+
+(defn query
+  [conf {h :host d :device pa :pri-adr sa :sec-adr} cmds wait repeat]
+  (let [ba  (byte-array 100)
+        ctrl (VXI11Factory/create h d)
         usr  (VXI11UserFactory/create)
-        rval1 (.connect ctrl usr)
-        dev  (.createDevice ctrl (:pri-adr adr) (:sec-adr adr))
-        rval2 (.connect dev usr)
-        rval3 (.remote dev usr)
-        rval4 (.write dev usr (.getBytes (first v)) (.length v))
-        rval5 (.read dev (byte-array 10) 50)
-        ]
-    (prn rval1)
-    (prn rval2)
-    (prn rval3)
-    (prn rval4)
-    (prn rval5)
-    (.disconnect dev)))
+        dev  (.createDevice ctrl pa sa)]
+    (.connect ctrl usr)
+    (.connect dev usr)
+    (.remote dev usr)
+    (.lock dev usr)
+    (let [data (mapv (fn [_]
+                       (let [v (mapv (fn [cmd]
+                                       (let [bc (.getBytes cmd)
+                                             lc (.length cmd)]
+                                         (.write dev usr bc lc)
+                                         (.read dev usr ba 100)
+                                         (String. ba)))
+                                     cmds)]
+                         (Thread/sleep wait)
+                         v))
+                     (range repeat))]
+      (.clear dev usr)
+      (.unlock dev usr)
+      (.local dev usr)
+      (.disconnect dev)
+      data)))
 
 (defn handler
   "Handles VXI11 queries.
@@ -56,10 +62,12 @@
   ```"
   [vxi-conf {w :Wait r :Repeat d :Device h :Host v :Value }]
   (if (and v h d )
-    (if-let [data (query h d
-                         (if (string? v) [v] v)
-                         (if w (u/number w) (:min-wait vxi-conf))
-                         (if r (u/number r) (:repeat vxi-conf)))]
-      {:data (u/meas-vec data)}
+    (let [dev-adr (parse-device-str d)
+          conn    (assoc dev-adr :host h)]
+      (if-let [data (query vxi-conf conn 
+                           (if (string? v) [v] v)
+                           (if w (u/number w) (:min-wait vxi-conf))
+                           (if r (u/number r) (:repeat vxi-conf)))]
+        {:data (u/meas-vec data)})
       {:error true :reason "no data"})
     {:error true :reason "missing <value>, <host> or <device>"}))
