b6938bc983527d1228e44c172833aebfe9ab0132
Thomas Bock <thomas.bock@ptb.de>
2020-12-10 07:18:31 +0100

    working modbus connection

--- src/devhub/modbus.clj
+++ src/devhub/modbus.clj
@@ -7,25 +7,29 @@
 
 (defn query
   [conf task]
-  
-  (let [config (.build (.setPort (ModbusTcpMasterConfig$Builder.) port))
-        master (ModbusTcpMaster. config)]))
+  (let [{host :Host addr :Address cmds :Value wait :Wait repeat :Repeat norep :NoReply} task
+        config (.build (.setPort (ModbusTcpMasterConfig$Builder. host) addr))
+        master (ModbusTcpMaster. config)]
+    (.connect master)
+    (prn (.getMeanRate  (.getResponseTimer master)))
+    (Thread/sleep 100)
+    (.disconnect master)))
 
 (defn safe
   [conf task]
-  (let [{h :Host a :Address q :Quantity f :FunctionCode w :Wait r :Repeat n :NoReply} task]
-    (when (and v a q f) (assoc task
-                               :Value   (if (string? v) [v] v)
-                               :Wait    (if w (u/number w) (:min-wait conf))
-                               :Repeat  (if r (u/number r) (:repeat conf))
-                               :NoReply (if n n false)))))
+  (let [{h :Host a :Address  f :FunctionCode w :Wait r :Repeat n :NoReply} task]
+    (when (and h a f) (assoc task
+                             :Address (u/number a)
+                             :Wait    (if w (u/number w) (:min-wait conf))
+                             :Repeat  (if r (u/number r) (:repeat conf))
+                             :NoReply (if n n false)))))
 (defn handler
   "Handles Modbus queries.
   
   Example:
   ```clojure
-  (def vc (:modbus (c/config)))
-  (handler vc {:Device \"gpib0,8\" :Host \"e75465\" :Value \":meas:func\"})
+   (def mc (:modbus (c/config)))
+  (handler mc {:Host \"172.30.56.46\" :Address 45407 :FunctionCode \"ReadHoldingRegisters\"})
   ```"
   [conf task]
   (if-let [task (safe conf task)]
