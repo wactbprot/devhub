40f0329ae2ddb677315d8520611cab2888c6f7ff
Thomas Bock <thomas.bock@ptb.de>
2020-12-11 15:45:02 +0100

    clean up modbus i

--- src/devhub/modbus.clj
+++ src/devhub/modbus.clj
@@ -1,52 +1,15 @@
 (ns devhub.modbus
   (:require [devhub.utils       :as u]
-            [devhub.conf        :as c]
-            [jdk.nio.ByteBuffer :as bb]
-            [jdk.nio.ByteOrder  :as bo])
+            [devhub.conf        :as c])
   (:import
    [com.intelligt.modbus.jlibmodbus Modbus]
    [com.intelligt.modbus.jlibmodbus.master ModbusMaster]
    [com.intelligt.modbus.jlibmodbus.master ModbusMasterFactory]
-   [com.intelligt.modbus.jlibmodbus.msg.request ReadHoldingRegistersRequest]
-   [com.intelligt.modbus.jlibmodbus.msg.request ReadInputRegistersRequest]
-   [com.intelligt.modbus.jlibmodbus.msg.response ReadHoldingRegistersResponse]
-   [com.intelligt.modbus.jlibmodbus.tcp TcpParameters]
-   [java.net InetAddress]
-   ))
-
-(defn br
-  "Builds the request."
-  [{a :default-slave-address} addr quant req]
-  (.setServerAddress req a)
-  (.setStartAddress req addr)
-  (.setQuantity req quant)
-  req)
-
-(defn ir [conf addr quant] (br conf addr quant (ReadInputRegistersRequest.)))
-
-(defn hr [conf addr quant] (br conf addr quant (ReadHoldingRegistersRequest.)))
-  
-(defn rir
-  "Executes a `ReadInputRegister` request. Returns the Integer
-  representation of `quant` registers."
-  [conf addr quant master]
-  (let [req (ir conf addr quant)
-        res (.getResponse req)]
-    (.processRequest master req)
-    (.getRegisters res)))
-
-(defn rhr
-  "Executes a `ReadHoldingRegister` request. Returns the Integer
-  representation of `quant` registers."
-  [conf addr quant master]
-  (let [req (hr conf addr quant)
-        res (.getResponse req)]
-    (.processRequest master req)
-    (.getRegisters res)))
+   [com.intelligt.modbus.jlibmodbus.tcp TcpParameters]))
 
 (defn query
   "Executes the query depending on the `FunctionCode`.
-  
+
   Example:
   ```clojure
   (def mc (:modbus (c/config)))
@@ -55,16 +18,25 @@
   (query mc {:Host \"e75480\" :Quantity 1 :Address 0 :FunctionCode :ReadInputRegisters})
   ```"
   [conf task]
-  (let [{host :Host fc :FunctionCode addr :Address quant :Quantity wait :Wait repeat :Repeat norep :NoReply} task
-        ip     (InetAddress/getByName host)
-        param  (TcpParameters. host (:port conf) (:keep-alive conf))
-        master (ModbusMasterFactory/createModbusMasterTCP param)]
+  (let [{host   :Host
+         fc     :FunctionCode
+         val    :Value
+         addr   :Address
+         quant  :Quantity
+         wait   :Wait
+         repeat :Repeat
+         norep  :NoReply} task
+        slave-addr (:default-slave-address conf)
+        param      (TcpParameters. host (:port conf) (:keep-alive conf))
+        master     (ModbusMasterFactory/createModbusMasterTCP param)]
     (Modbus/setAutoIncrementTransactionId true)
     (.connect master)
     (let [data (condp = fc
-                 :ReadHoldingRegisters (rhr conf addr quant master)
-                 :ReadInputRegisters   (rir conf addr quant master)
-                 )]
+                 :ReadHoldingRegisters (.readHoldingRegisters master slave-addr addr quant)
+                 :ReadInputRegisters   (.readInputRegisters   master slave-addr addr quant)
+                 :ReadCoils            (.readCoils            master slave-addr addr quant)
+                 :ReadDiscreteInputs   (.readDiscreteInputs   master slave-addr addr quant)
+                 :writeSingleRegister  (.writeSingleRegister  master slave-addr addr val))]
       (.disconnect master)
       data)))
 
@@ -72,15 +44,16 @@
   [conf task]
   (let [{h :Host a :Address q :Quantity fc :FunctionCode w :Wait r :Repeat n :NoReply} task]
     (when (and h a fc q) (assoc task
+                                :FunctionCode (keyword fc)
                                 :Address      (u/number a)
                                 :Quantity     (u/number q)
-                                :FunctionCode (keyword fc)
                                 :Wait         (if w (u/number w) (:min-wait conf))
                                 :Repeat       (if r (u/number r) (:repeat conf))
                                 :NoReply      (if n n false)))))
+
 (defn handler
   "Handles Modbus queries.
-  
+
   Example:
   ```clojure
    (def mc (:modbus (c/config)))
@@ -92,6 +65,3 @@
       {:data (u/meas-vec data)}
       {:error true :reason "no data"})
     {:error true :reason "missing <value>, <host> or <device>"}))
-
-
-
