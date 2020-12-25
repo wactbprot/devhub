43237555d18c68767213cfb6657647c51f7b6be4
Wact B. Prot <wactbprot@gmail.com>
2020-12-25 22:36:12 +0100

    .

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,6 +7,7 @@ echo "rm -r target/default/doc/*\n"
 
 echo "=======  graphviz  =========\n\n"
 dot -Tpng ./resources/req-res.dot > ./resources/req-res.png
+cp ./resources/req-res.png ./docs
 
 echo "=========  codox   =========\n\n"
 lein codox
