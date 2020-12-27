ebf18dc05ff544a1667d888626d219f37c9dd6ec
Wact B. Prot <wactbprot@gmail.com>
2020-12-27 15:39:15 +0100

    dot

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,10 +4,11 @@ echo "========= clean up: ========\n"
 rm -r target/default/doc/*
 echo "rm -r target/default/doc/*\n"
 
-echo "=======  graphviz  =========\n\n"
+echo "=======  graphviz  =========\n"
 dot -Tsvg ./docs/req-res.dot > ./docs/req-res.svg
-echo "dot -Tsvg ./docs/req-res.dot > ./docs/req-res.svg"
+echo "dot -Tsvg ./docs/req-res.dot > ./docs/req-res.svg\n"
 
-echo "=========  codox   =========\n\n"
+echo "=========  codox   =========\n"
 lein codox
 cp -r target/default/doc/* docs/api
+echo "cp -r target/default/doc/* docs/api\n"
