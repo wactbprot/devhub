0d9b1d6e8cad79b59e6a9760e1e8b2359e3083a2
Wact B. Prot <wactbprot@gmail.com>
2020-12-28 22:50:49 +0100

    safe refactor

---
+++ test/devhub/utils_test.clj
@@ -0,0 +1,12 @@
+(ns devhub.utils-test
+  (:require [clojure.test :refer :all]
+            [devhub.utils :refer :all]))
+
+(deftest meas-vec-i
+  (testing "returns expected (i)"
+    (is (map? (meas-vec {}))
+        "map")
+    (is (nil? (meas-vec nil))
+        "nil")
+    (is (empty? (:_x (meas-vec [])))
+        "nil")))
