705c04b334259ef2c88c60faed09a2ef8300138f
Wact B. Prot <wactbprot@gmail.com>
2020-12-29 14:37:46 +0100

    pp-str tests

--- test/devhub/js_pp_test.clj
+++ test/devhub/js_pp_test.clj
@@ -1,7 +1,7 @@
 (ns devhub.js-pp-test
   (:require [clojure.test :refer :all]
-            [devhub.js-pp :refer :all]
-            ))
+            [devhub.js-pp :refer :all]))
+
 (def pp [
          "var _vec=_x.map(function(s){return s.split(' ')[1]}).map(parseFloat),",
          "_res = _.vlStat(_.checkNumArr(_vec).Arr),",
@@ -15,7 +15,13 @@
          "var LogData={'vec':_vec, 't_start':_t_start, 't_stop':_t_stop}"
          ])
 
+(def data {:_x ["foo 2.3" "foo 2.3" "foo 2.3"]
+           :_t_start [1 2 3]
+           :_t_stop [1 2 3]})
+  
 (deftest js-pp-i
   (testing " returns nil (i)"
     (is (nil? (pp-str nil nil))
-        "nil .")))
+        "nil .")
+    (is (string? (pp-str pp data))
+        "string")))
