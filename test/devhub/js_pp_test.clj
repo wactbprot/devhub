989155d3d8500f1527cc35abe2ff5481cbe7b496
Wact B. Prot <wactbprot@gmail.com>
2020-12-30 16:43:33 +0100

    PostScript tests i

--- test/devhub/js_pp_test.clj
+++ test/devhub/js_pp_test.clj
@@ -3,8 +3,7 @@
             [devhub.js-pp :refer :all]
             [devhub.utils :as u]))
 
-(def pp [
-         "var _vec=_x.map(function(s){return s.split(' ')[1]}).map(parseFloat),",
+(def pp [ "var _vec=_x.map(function(s){return s.split(' ')[1]}).map(parseFloat),",
          "_res = _.vlStat(_.checkNumArr(_vec).Arr),",
          "ToExchange={",
          "'@exchpath.Value':_res.mv,",
@@ -13,10 +12,9 @@
          "'@exchpath.Unit':'@unit',",
          "'@exchpath.Type':'@token'",
          "};",
-         "var LogData={'vec':_vec, 't_start':_t_start, 't_stop':_t_stop}"
-         ])
+         "var LogData={'vec':_vec, 't_start':_t_start, 't_stop':_t_stop}"])
 
-(def data {:_x ["foo 2.3" "foo 2.3" "foo 2.3"]
+(def data {:_x ["foo 1.1" "foo 2.2" "foo 3.3"]
            :_t_start [1 2 3]
            :_t_stop [1 2 3]})
   
