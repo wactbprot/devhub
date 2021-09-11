0be87c34c8d908644738f9c7bace9efe484d0182
Wact. B. Prot <wactbprot@gmail.com>
2021-09-11 20:02:04 +0200

    clean pp-scripts.daq34970, untested

--- src/devhub/pp_scripts/daq34970.clj
+++ src/devhub/pp_scripts/daq34970.clj
@@ -1,17 +1,16 @@
 (ns devhub.pp-scripts.daq34970
+  ^{:author "Wact B. Prot <wactbprot@gmail.com>"
+    :doc "PostProcessing for a DAQ34970."}
   (:require [clojure.string  :as string]
             [devhub.pp-utils :as ppu]
-            [devhub.utils    :as u]))
+            [devhub.utils :as u]))
 
 (defn split-line [s] (string/split s #","))
 
 (defn count-ok? [v n] (= (count v) n))
 
-(defn temperature-scanner-read-out
-  [task]
-  (let [input (:PostScriptInput task)
-        chan  (:Channels input) prefix (:Prefix input) suffix (:Suffix input)
-        n     (count chan)
-        v     (mapv  (fn [l] (mapv u/number (split-line l))) (:_x task))
-        v     (ppu/transpose (filterv (fn [v] (count-ok? v n)) v))]
+(defn temperature-scanner-read-out [{{chan :Channels prefix :Prefix suffix :Suffix} :PostScriptInput x :_x :as task}]
+  (let [n (count chan)
+        v (mapv  #(mapv u/number (split-line %)) x)
+        v (ppu/transpose (filterv #(count-ok? % n) v))]
     (merge task {:Result (mapv (fn [ch v] (ppu/vl-result (str prefix ch suffix) v "C")) chan v)})))
