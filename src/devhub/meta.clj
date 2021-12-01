90b6bc8c956ae8b8293f06f0067b2db7958bc395
Thomas Bock <thomas.bock@ptb.de>
2021-12-01 12:57:28 +0100

    meta in case of jar

--- src/devhub/meta.clj
+++ src/devhub/meta.clj
@@ -4,4 +4,6 @@
   (:require [clojure.tools.build.api :as b]))
 
 
-(def version (format "0.16.%s" (b/git-count-revs nil)))
+(def version (format "0.16.%s" (try
+                                 (b/git-count-revs nil)
+                                 (catch Exception e "x"))))
